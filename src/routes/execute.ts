cb36349b7aff1c984a2df97b1de97bfac1e7d176
Poojith P <poojithpagadekal26@gmail.com>
2026-03-07 14:55:18 +0530

    refactor: remove unused imports from execute route

--- src/routes/execute.ts
+++ src/routes/execute.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from "express";
-import { executeCode } from "../executor/index";
 import executionQueue from "../queue/executionQueue";
 
 const router = Router();
